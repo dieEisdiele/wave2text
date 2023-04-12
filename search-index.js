6f51b6dbd914e925a00eb48d0d887b138c9e5a07
Tom Su <dieeisdiele.ts@gmail.com>
2023-04-12 21:59:41 +1000

    -set up documentation

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"wave2text":{"doc":"","t":[5],"n":["main"],"q":["wave2text"],"d":[""],"i":[0],"f":[[[]]],"p":[]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
---
+++ source-files.js
@@ -0,0 +1,4 @@
+var sourcesIndex = JSON.parse('{\
+"wave2text":["",[],["main.rs"]]\
+}');
+createSourceSidebar();
