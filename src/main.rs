b077647deda210d866d726091a8dcc9ecf95472c
Tom Su <dieeisdiele.ts@gmail.com>
2023-04-16 00:15:50 +1000

    menu navigation is almost fully working

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,7 @@ fn main() {
             default
         },
     };
+    let mut sample_rate_hz: f64 = settings.sample_rate_hz;
     let pulse: Vec<f64> = match get_pulse_shape(&settings.pulse_path) {
         Ok(vec) => vec,
         Err(error) => panic!("error loading pulse shape from file: {}", error),
@@ -43,19 +44,33 @@ fn main() {
 
     // Define vectors to store waveform in
     let mut waveform: Vec<f64> = Vec::new();
-    let mut wave_history: Vec<&str> = Vec::new();
+    let mut wave_history: Vec<String> = Vec::new();
 
     // Main program loop
     loop {
         match terminal_menu() {
-            1 => println!("Edit waveform."),
-            2 => if confirm("clear the current waveform") == true {
+            1 => {
+                loop {
+                    println!("\nSampling rate (Hz): {}", sample_rate_hz);
+                    if confirm("Is this correct? Enter [Y] to confirm, or press any other button to enter a different sampling rate.") {
+                        break
+                    } else {
+                        // TODO Query user to enter new sample rate
+                        // TODO Query user if the new sample rate should be saved as default
+                        continue
+                    };
+                }
+                let pulse_temp = pulse.to_vec();
+                (waveform, wave_history) = edit_waveform(wave_history, waveform, pulse_temp, sample_rate_hz);
+            },
+            2 => println!("View waveform history."),
+            3 => if confirm("Are you sure you want to clear the current waveform? Enter [Y] to confirm, or press any other button to return to the menu.") {
                 waveform.clear();
                 wave_history.clear();
                 println!("Waveform cleared.");
             } else {continue},
-            3 => println!("Save waveform."),
-            4 => if confirm("exit the program") == true {
+            4 => println!("Save waveform."),
+            5 => if confirm("Are you sure you want to exit the program? Enter [Y] to confirm, or press any other button to return to the menu.") {
                 println!("Exiting...");
                 break
             } else {continue},
@@ -91,9 +106,10 @@ fn terminal_menu() -> u8 {
 What would you like to do?
     
     [1]. Edit waveform.
-    [2]. Clear waveform.
-    [3]. Save waveform.
-    [4]. Exit program.
+    [2]. View waveform history.
+    [3]. Clear waveform.
+    [4]. Save waveform.
+    [5]. Exit program.
     "#;
     let input_prompt: &str = "Please enter a number 1-4.";
 
@@ -111,7 +127,7 @@ What would you like to do?
         }
 
         match input.trim().parse::<u8>() {
-            Ok(num) => if num > 0 && num < 5 {
+            Ok(num) => if num > 0 && num < 6 {
                 return num
             } else {
                     println!("error: number outside valid range");
@@ -128,14 +144,89 @@ What would you like to do?
 }
 
 /// User menu for editing the current waveform.
+fn edit_waveform(wave_history_pre: Vec<String>, waveform_pre: Vec<f64>, pulse_shape: Vec<f64>, sample_rate_hz: f64) -> (Vec<f64>, Vec<String>) {
+    let (pulse_frequency_hz, duration_sec): (f64, f64) = loop {
+        let (pulse_frequency_hz_temp, duration_sec_temp): (f64, f64) = get_wave_variables();
+        println!("\nPulse frequency: {} Hz", pulse_frequency_hz_temp);
+        println!("Duration: {} s", duration_sec_temp);
+        if confirm("Are these parameters correct? Enter [Y] to confirm, or press any other button to re-enter them.") {
+            break (pulse_frequency_hz_temp, duration_sec_temp)
+        } else {
+            continue
+        }
+    };
 
+    let waveform_new: Vec<f64> = wave_gen(waveform_pre, pulse_shape, sample_rate_hz, pulse_frequency_hz, duration_sec);
+
+    let mut wave_history: Vec<String> = Vec::from(wave_history_pre);
+    let wave_history_new: String = format!("    Sampling rate:   {} Hz\n    Pulse frequency: {} Hz\n    Duration:        {} s\n\n", sample_rate_hz, pulse_frequency_hz, duration_sec);
+    wave_history.push(wave_history_new);
+
+    return (waveform_new, wave_history)
+}
+
+/// Get user-defined variables
+fn get_wave_variables() -> (f64, f64) {
+    let input_prompt: &str = "Please enter a positive number.";
+
+    let mut pulse_frequency_hz = String::new();
+    let mut duration_sec = String::new();
+
+    println!("\nPulse phase (Hz)");
+    let pulse_frequency_hz: f64 = loop {
+        match io::stdin().read_line(&mut pulse_frequency_hz) {
+            Ok(_) => (),
+            Err(error) => {
+                println!("error: {}", error);
+                println!("{}", input_prompt);
+                pulse_frequency_hz.clear();
+                continue;
+            },
+        }
+
+        match pulse_frequency_hz.trim().parse::<f64>() {
+            Ok(num) => break num,
+            Err(error) => {
+                println!("error: {}", error);
+                println!("{}", input_prompt);
+                pulse_frequency_hz.clear();
+                continue;
+            },
+        };
+    };
+
+    println!("Duration (s)");
+    let duration_sec: f64 = loop {
+        match io::stdin().read_line(&mut duration_sec) {
+            Ok(_) => (),
+            Err(error) => {
+                println!("error: {}", error);
+                println!("{}", input_prompt);
+                duration_sec.clear();
+                continue;
+            },
+        }
+
+        match duration_sec.trim().parse::<f64>() {
+            Ok(num) => break num,
+            Err(error) => {
+                println!("error: {}", error);
+                println!("{}", input_prompt);
+                duration_sec.clear();
+                continue;
+            },
+        };
+    };
+
+    return (pulse_frequency_hz, duration_sec)
+}
 
 /// Constructs a new waveform in which the provided pulse repeats as specified, and appends it to the end of the existing waveform.
-fn wave_gen(waveform_pre: Vec<f64>, pulse_shape: Vec<f64>, sample_rate_hz: f64, phase_hz: f64, duration_sec: f64) -> Vec<f64> {
+fn wave_gen(waveform_pre: Vec<f64>, pulse_shape: Vec<f64>, sample_rate_hz: f64, pulse_frequency_hz: f64, duration_sec: f64) -> Vec<f64> {
     let mut waveform: Vec<f64> = Vec::from(waveform_pre);
 
-    let period_sec: f64 = 1.0/phase_hz;
-    let pulse_count_final = (phase_hz * duration_sec).ceil() as u32;
+    let period_sec: f64 = 1.0/pulse_frequency_hz;
+    let pulse_count_final = (pulse_frequency_hz * duration_sec).ceil() as u32;
     let wave_len_final: f64 = (sample_rate_hz * duration_sec).round();
 
     let mut waveform_new: Vec<f64> = Vec::new();
@@ -143,7 +234,7 @@ fn wave_gen(waveform_pre: Vec<f64>, pulse_shape: Vec<f64>, sample_rate_hz: f64, 
         waveform_new.extend(&pulse_shape);
 
         let wave_len_target = f64::min(wave_len_final, (period_sec * sample_rate_hz * (pulse_count as f64)).round()) as usize;
-        let zeros: Vec<f64> = vec![0.0; wave_len_target - waveform_new.len()];
+        let zeros: Vec<f64> = vec![0.0; wave_len_target - waveform_new.len()];          // ! This calculation is broken
         waveform_new.extend(zeros)
     }
 
@@ -151,9 +242,9 @@ fn wave_gen(waveform_pre: Vec<f64>, pulse_shape: Vec<f64>, sample_rate_hz: f64, 
     waveform
 }
 
-/// Confirms that the user wants to exit.
-fn confirm(action: &str) -> bool {
-    println!("Are you sure you want to {}? Enter [Y] to confirm, or press any other button to return to the menu.", action);
+/// Prompts user to confirm action before proceeding.
+fn confirm(query: &str) -> bool {
+    println!("{}", query);
 
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
